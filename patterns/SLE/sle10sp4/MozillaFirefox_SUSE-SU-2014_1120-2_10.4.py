c55921586abebadefe49f44e25536d0aa32d7939
Jason Record <g23guy@gmail.com>
2014-09-23 16:32:53 -0600

    new patterns

---
+++ patterns/SLE/sle10sp4/MozillaFirefox_SUSE-SU-2014_1120-2_10.4.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for MozillaFirefox SUSE-SU-2014:1120-2
+# Description: Security fixes for SUSE Linux Enterprise 10 SP4 LTSS
+# Source:      Security Announcement Parser v1.0.1
+# Modified:    2014 Sep 23
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "MozillaFirefox"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-09/msg00014.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'MozillaFirefox'
+MAIN = 'MozillaFirefox'
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:1120-2'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 10):
+	if ( SERVER['DistroPatchLevel'] == 4 ):
+		PACKAGES = {
+			'firefox-gtk2': '2.18.9-0.11.1',
+			'mozilla-nss-devel': '3.16.4-0.5.2',
+			'firefox-gtk2-lang': '2.18.9-0.11.1',
+			'firefox-gtk2-32bit': '2.18.9-0.11.1',
+			'mozilla-nspr': '4.10.7-0.5.1',
+			'mozilla-nss-32bit': '3.16.4-0.5.2',
+			'mozilla-nss-tools': '3.16.4-0.5.2',
+			'MozillaFirefox-translations': '24.8.0esr-0.5.1',
+			'mozilla-nspr-devel': '4.10.7-0.5.1',
+			'MozillaFirefox': '24.8.0esr-0.5.1',
+			'mozilla-nss': '3.16.4-0.5.2',
+			'mozilla-nspr-32bit': '4.10.7-0.5.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
